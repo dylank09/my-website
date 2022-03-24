5a8a86451ec6b8b0d06c776d4b916b3c162e0594
Dylan <dylank09@gmail.com>
2022-03-24 13:27:28 +0000

    added react app and github action to push to hosting server via ftp

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img
+          src="https://www.petmd.com/sites/default/files/styles/article_image/public/CANS_dogsmiling_379727605.jpg?itok=E-nD1tXb"
+          className="App-logo"
+          alt="logo"
+        />
+        <p>Successfully hosting on 000webhosting</p>
       </header>
     </div>
   );
