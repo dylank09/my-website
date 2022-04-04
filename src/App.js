762aedca74b480aba0a83a70ba0153f99cb293aa
Dylan <dylank09@gmail.com>
2022-04-04 21:37:21 +0100

    made custom sidebar, rearranged dark switch

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./App.css";
 import useLocalStorage from "use-local-storage";
 import { FaSun, FaRegMoon } from "react-icons/fa";
+import darkLogo from "./assets/dk_button.svg";
+import lightLogo from "./assets/dk_button_inverted.svg";
 
 import ReactSwitch from "react-switch";
 import SideBar from "./components/SideBar.js";
@@ -20,41 +22,61 @@ function App() {
 
   return (
     <div className="App" data-theme={theme}>
-      <div className="dark-mode-container">
-        <span>Dark Mode</span>
-        <ReactSwitch
-          className="dark-mode-switch"
-          onChange={switchTheme}
-          checked={theme === "dark"}
-          uncheckedIcon={
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: "100%",
-              }}
-            >
-              <FaSun color="white" width="100%" height="100%" />
-            </div>
-          }
-          checkedIcon={
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: "100%",
-              }}
-            >
-              <FaRegMoon color="white" width="100%" height="100%" />
-            </div>
-          }
-          offColor="#e68a00"
-          onColor="#000000"
-        />
+      <div className="sidebar">
+        {theme === "dark" ? (
+          <img
+            id="logo"
+            className="logo-image"
+            src={darkLogo}
+            alt="website logo"
+          />
+        ) : (
+          <img
+            id="logo"
+            className="logo-image"
+            src={lightLogo}
+            alt="website logo"
+          />
+        )}
+        <SideBar />
+        <div className="dark-mode-container">
+          <hr className="sidebar-break"></hr>
+          <div>
+            <span>Dark Mode</span>
+            <ReactSwitch
+              className="dark-mode-switch"
+              onChange={switchTheme}
+              checked={theme === "dark"}
+              uncheckedIcon={
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: "100%",
+                  }}
+                >
+                  <FaSun color="white" width="100%" height="100%" />
+                </div>
+              }
+              checkedIcon={
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: "100%",
+                  }}
+                >
+                  <FaRegMoon color="white" width="100%" height="100%" />
+                </div>
+              }
+              offColor="#e68a00"
+              onColor="#000000"
+            />
+          </div>
+        </div>
       </div>
-      <SideBar />
     </div>
   );
 }
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,18 +1,15 @@
-import { ProSidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
-import "react-pro-sidebar/dist/css/styles.css";
-import { FaGem, FaHeart } from "react-icons/fa";
+// import { FaGem, FaHeart, FaGithub } from "react-icons/fa";
+// import { useState } from "react";
+import "./SideBar.css";
 
 const SideBar = () => {
   return (
-    <ProSidebar>
-      <Menu iconShape="square">
-        <MenuItem icon={<FaGem />}>Dashboard</MenuItem>
-        <SubMenu title="Components" icon={<FaHeart />}>
-          <MenuItem>Component 1</MenuItem>
-          <MenuItem>Component 2</MenuItem>
-        </SubMenu>
-      </Menu>
-    </ProSidebar>
+    <div className="nav-links-container">
+      <div className="nav-link">Home</div>
+      <div className="nav-link">Projects</div>
+      <div className="nav-link">About</div>
+      <div className="nav-link">Contact</div>
+    </div>
   );
 };
 
